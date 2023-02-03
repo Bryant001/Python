eb401a7ecfdd0b8173024f6da73a3627023e458c
John Paul Bryant <Bryant001@users.noreply.github.com>
2023-02-03 14:50:55 -0600

    Update sbom.py

--- sbom.py
+++ sbom.py
@@ -1,16 +1,21 @@
 import sys
 import requests
+import json
 from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 
 
 api_base = "https://api.veracode.com/appsec/v1"
+sca_base = "https://api.veracode.com/srcclr"
 headers = {"User-Agent": "Python HMAC Example"}
-
+app_name = "verademo_dotnet"
 
 if __name__ == "__main__":
-
+    if not app_name:
+        query_name = ""
+    else:
+        query_name = "?name=" + app_name
     try:
-        response = requests.get(api_base + "/applications", auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
+        response = requests.get(api_base + "/applications" + query_name, auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
     except requests.RequestException as e:
         print("Whoops!")
         print(e)
@@ -18,11 +23,18 @@ if __name__ == "__main__":
 
     if response.ok:
         data = response.json()
-        data_file = open("csvTest.csv", "w", newline='') #this can be any file type but I chose to write to a CSV file.
+        data_file = open("SBOM.json", "w", newline='')
         for app in data["_embedded"]["applications"]:
             print(app["profile"]["name"]) # prints the app names to the console/terminal
-            data_file.write(app["profile"]["name"]+'\n') # appends the app name to the file with a newline character
-         
+            print(app["guid"])
+            try:
+                response2 = requests.get(sca_base + "/sbom/v1/targets/" + app["guid"] + "/cyclonedx?type=application&linked=true", auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
+                sbom = response2.json()
+                json.dump(sbom, data_file, indent=4)
+            except requests.RequestException as e:
+                print("Whoops!")
+                print(e)
+                sys.exit(1)
         data_file.close()
     else:
         print(response.status_code)
