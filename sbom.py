144029c18be62b568f88ecbaec590e1b2f6ca3b6
John Paul Bryant <Bryant001@users.noreply.github.com>
2023-02-03 14:52:31 -0600

    Update sbom.py

--- sbom.py
+++ sbom.py
@@ -23,18 +23,18 @@ if __name__ == "__main__":
 
     if response.ok:
         data = response.json()
-        data_file = open("SBOM.json", "w", newline='')
+        #data_file = open("SBOM.json", "w", newline='')
         for app in data["_embedded"]["applications"]:
             print(app["profile"]["name"]) # prints the app names to the console/terminal
             print(app["guid"])
             try:
                 response2 = requests.get(sca_base + "/sbom/v1/targets/" + app["guid"] + "/cyclonedx?type=application&linked=true", auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
                 sbom = response2.json()
-                json.dump(sbom, data_file, indent=4)
+                return json.dump(sbom, indent=4)
             except requests.RequestException as e:
                 print("Whoops!")
                 print(e)
                 sys.exit(1)
-        data_file.close()
+        #data_file.close()
     else:
         print(response.status_code)
